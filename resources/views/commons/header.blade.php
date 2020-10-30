9574e5a845b3126872f26c51535a0d50762ad985
arytossy <arytossy@gmail.com>
2020-10-30 07:04:46 +0000

    Set up view layout

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        
+        <a class="navbar-brand" href="/">
+            <i class="fas fa-tasks"></i> Tasklist
+        </a>
+        
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar-nav">
+            <ul class="navbar-nav mx-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">リンク</a>
+                </li>
+            </ul>
+        </div>
+        
+    </nav>
+</header>
